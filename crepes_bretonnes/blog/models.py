d94c649801cb1c94873f7690a5b1a2c48942e08f
Anthony <a.dottesi@hotmail.fr>
2020-07-16 13:58:36 +0200

    les modèles

--- crepes_bretonnes/blog/models.py
+++ crepes_bretonnes/blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Article(models.Model):
+    titre = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100)
+    auteur = models.CharField(max_length=42)
+    contenu = models.TextField(null=True)
+    date = models.DateTimeField(default=timezone.now,
+                                verbose_name="Date de parution")
+
+
+    class Meta:
+        ordering = ['date']
+
+    def __str__(self):
+        return self.titre
--- crepes_bretonnes/blog/urls.py
+++ crepes_bretonnes/blog/urls.py
@@ -2,11 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('accueil', views.home),
-    path('article/<id_article>', views.view_article, name="afficher_article"),
+    path('home', views.home),
     path('articles/<int:year>/', views.list_articles),
     path('articles/<int:year>/<int:month>', views.list_articles),
     path('date', views.date_actuelle),
-    path('addition/<int:nombre1>/<int:nombre2>/', views.addition)
+    path('addition/<int:nombre1>/<int:nombre2>/', views.addition),
+    path('accueil', views.accueil, name="accueil"),
+    path('article/<int:id>-<slug:slug>$', views.lire, name='lire'),
 ]
 
